7d74fbb8fbb75fcb53115843eb13756afb487292
Yosiak-alv <Josias_romero60@outlook.com>
2023-08-17 17:15:35 -0600

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -12,8 +12,8 @@ import SocialIcon from './SocialIcon.vue';
                 </div>
                 <span class='navbar-text'>
                     <div class="social-icon">
-                        <SocialIcon :href="'https://www.instagram.com/sportc.sv/'" :src="'/src/assets/insta.svg'" :alt="'Insta'"/>
-                        <SocialIcon :href="'https://www.facebook.com/profile.php?id=100054284840887'" :src="'/src/assets/facebook.svg'" :alt="'Face'"/>
+                        <SocialIcon :href="'https://www.instagram.com/sportc.sv/'" src="/src/assets/insta.svg" :alt="'Insta'"/>
+                    <SocialIcon :href="'https://www.facebook.com/profile.php?id=100054284840887'" src="/src/assets/facebook.svg" :alt="'Face'"/>
                     </div>
                 </span>
             </div>
