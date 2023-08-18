213e2729126909c257d2c8748131e0eda60eadac
Yosiak-alv <Josias_romero60@outlook.com>
2023-08-17 20:22:12 -0600

    Update Pricing.vue

--- src/components/sections/Pricing.vue
+++ src/components/sections/Pricing.vue
@@ -12,37 +12,7 @@
                     dé el primer paso hacia una vida más saludable y feliz. Tu aventura esta a punto de empezar.
                 </p>
             </div>
-            <div class="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
-                <!-- Pricing Card -->
-                <div class="flex flex-col p-6 mx-auto max-w-lg text-center rounded-lg border shadow border-gray-600 xl:p-8 bg-gray-800 text-white">
-                    <h3 class="mb-4 text-2xl font-semibold">Pesas</h3>
-                    <p class="font-light sm:text-lg text-gray-400">Para los amantes de las pesas, has llegado al lugar adecuado.</p>
-                    <div class="flex justify-center items-baseline my-8">
-                        <span class="mr-2 text-5xl font-extrabold">$12</span>
-                        <span class="text-gray-400">/mes</span>
-                    </div>
-                    <!-- List -->
-                    <ul role="list" class="mb-8 space-y-4 text-left">
-                        <li class="flex items-center space-x-3">
-                            <!-- Icon -->
-                            <svg class="flex-shrink-0 w-5 h-5 text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-                            <span>Acceso a todas las máquinas</span>
-                        </li>
-                        <li class="flex items-center space-x-3">
-                            <!-- Icon -->
-                            <svg class="flex-shrink-0 w-5 h-5 text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-                            <span>Acceso a cualquier hora (dentro del horario)</span>
-                        </li>
-                        <li class="flex items-center space-x-3">
-                            <!-- Icon -->
-                            <svg class="flex-shrink-0 w-5 h-5 text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-                            <span>Area exclusiva de Pesas y maquinas de Fuerza</span>
-                        </li>
-                       
-                       
-                    </ul>
-                </div>
-
+            <div class="space-y-8 lg:grid lg:grid-cols-2 sm:gap-6 xl:gap-10 lg:space-y-0">
                 <!-- Pricing Card -->
                 <div class="flex flex-col p-6 mx-auto max-w-lg text-center rounded-lg border shadow border-gray-600 xl:p-8 bg-gray-800 text-white">
                     <h3 class="mb-4 text-2xl font-semibold">Full-Access</h3>
@@ -56,7 +26,7 @@
                         <li class="flex items-center space-x-3">
                             <!-- Icon -->
                             <svg class="flex-shrink-0 w-5 h-5 text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-                            <span>Acceso a todas las máquinas</span>
+                            <span>Acceso a todas las Instalaciones</span>
                         </li>
                         <li class="flex items-center space-x-3">
                             <!-- Icon -->
@@ -81,13 +51,13 @@
                         
                     </ul>
                 </div>
-
+                
                 <!-- Pricing Card -->
                 <div class="flex flex-col p-6 mx-auto max-w-lg text-center rounded-lg border shadow border-gray-600 xl:p-8 bg-gray-800 text-white">
                     <h3 class="mb-4 text-2xl font-semibold">Diario</h3>
-                    <p class="font-light sm:text-lg text-gray-400">Buen punto de partida diseñada para impulsarte hacia el éxito.</p>
+                    <p class="font-light sm:text-lg text-gray-400">Excelente punto de partida creado para <br>aquellos que no disponen de mucho tiempo.</p>
                     <div class="flex justify-center items-baseline my-8">
-                        <span class="mr-2 text-5xl font-extrabold">$2</span>
+                        <span class="mr-2 text-5xl font-extrabold">$3</span>
                         <span class="text-gray-400">/dia</span>
                     </div>
                     <!-- List -->
@@ -95,12 +65,13 @@
                         <li class="flex items-center space-x-3">
                             <!-- Icon -->
                             <svg class="flex-shrink-0 w-5 h-5 text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-                            <span>Acceso a todas las máquinas</span>
+                            <span>Acceso a todas las Instalaciones</span>
                         </li>
                         
                     </ul>
                 </div>
             </div>
-        </div>
+         </div>
+       
     </section>
 </template>
